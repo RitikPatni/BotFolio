b8ac1721e1ee31d1d0b30ef6d88cffd67d2e7e32
Ritik Patni <patniritik@proton.me>
2026-05-24 20:00:10 +0530

    chore: checkpoint favicon and ui refinements

---
+++ src/config/site.ts
@@ -0,0 +1,38 @@
+export type SocialProfile = {
+  label: string;
+  href: string;
+  icon: string;
+  iconSource: 'iconsvg.xyz';
+};
+
+export const SITE_CONFIG = {
+  personName: 'Ritik Patni',
+  defaultDescription: 'Ritik Patni - frontend developer and wildlife/macro photographer.',
+  rssTitle: 'Ritik Patni RSS Feed',
+  socials: [
+    {
+      label: 'GitHub',
+      href: 'https://github.com/RitikPatni',
+      icon: 'github',
+      iconSource: 'iconsvg.xyz'
+    },
+    {
+      label: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/ritikpatni/',
+      icon: 'linkedin',
+      iconSource: 'iconsvg.xyz'
+    },
+    {
+      label: 'Instagram',
+      href: 'https://www.instagram.com/ritikpatni/',
+      icon: 'instagram',
+      iconSource: 'iconsvg.xyz'
+    },
+    {
+      label: 'CSSBattle',
+      href: 'https://cssbattle.dev/player/yJin3lFzHMfkagr5R9DXYf336Jr1',
+      icon: 'code',
+      iconSource: 'iconsvg.xyz'
+    }
+  ] as SocialProfile[]
+};
